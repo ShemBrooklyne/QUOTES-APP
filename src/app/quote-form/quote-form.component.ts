3456fb99f4209897d72c4e040beba69a45d0a8cb
ShemBrooklyne <shemnyatti@gmail.com>
2020-08-23 19:52:45 +0300

    Added upvote and down vote buttons

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,7 +7,7 @@ import { Quotes } from '../quotes';
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuotes = new Quotes(0,"","",new Date());
+  newQuotes = new Quotes("","",new Date());
   @Output() addQuotes = new EventEmitter<Quotes>();
 
     submitQuotes() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,13 +9,20 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
 
   quote:Quotes[] = [
-    new Quotes (1, 'Build in silence and let your success make the noise for you','Brooklyne', new Date(2020,8,21)),
-    new Quotes (2, 'There is always light at the end of the tunnel. Never lose focus!', 'Shem', new Date(2020,4,12)),
+    new Quotes ('Build in silence and let your success make the noise for you','Brooklyne', new Date(2020,7,21)),
+    new Quotes ('There is always light at the end of the tunnel. Never lose focus!', 'Shem', new Date(2020,4,12)),
+    new Quotes ('Never stop dreaming. A dream is what it takes to achieve.', 'Montgamery', new Date(2010,4,5)),
+    new Quotes ('Act as if what you do makes a difference. It does.', 'William James', new Date(2006,7,8)),
+    new Quotes ('Success is not final, failure is not fatal: it is the courage to continue that counts.', 'Winston Churchill', new Date(1985,2,1)),
+    new Quotes ('Never bend your head. Always hold it high. Look the world straight in the eye.','Hellen Keller', new Date(2000,1,8)),
+    new Quotes ('What you get by achieving your goals is not as important as what you become by achieving your goals.', 'Zig Ziglar', new Date(2009,9,4)),
+    new Quotes ('Believe you can and you are halfway there.', 'Theodore Roosevelt', new Date(1937,6,2))
+
   ];
 
   deleteQuote(isComplete, index) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}?`)
+      let toDelete = confirm(`Are you sure you want to delete this quote by ${this.quote[index].author}?`)
 
       if (toDelete) {
         this.quote.splice(index,1)
