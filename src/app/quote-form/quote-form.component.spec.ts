0e5fb5d0cd5b0c21a2a95feb289772d65273938a
ShemBrooklyne <shemnyatti@gmail.com>
2021-01-19 13:46:21 +0300

    stash

--- src/app/quote-form/quote-form.component.spec.ts
+++ src/app/quote-form/quote-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { QuoteFormComponent } from './quote-form.component';
 
@@ -6,7 +6,7 @@ describe('QuoteFormComponent', () => {
   let component: QuoteFormComponent;
   let fixture: ComponentFixture<QuoteFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ QuoteFormComponent ]
     })
